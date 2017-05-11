75f7ae81b1b8d2f26c8bcbfaec8673d40842ca58
Şamil NART <samil.nart@gmail.com>
2017-05-11 20:08:32 +0300

    Fixed aether's commit, bot now uses config files for token storage.

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
--import * as Discord from 'discord.js';
- -import * as log from 'winston';
- -
- -const client = new Discord.Client();
- -
- -client.on('ready', () => {
- -    log.info(`Client is ready, username: ${client.user.username}.`);
- -});
- -
- -client.on('message', (msg) => {
- -    if (msg.content === 'ping') {
- -        msg.channel.sendMessage('pong');
- -    }
- -});
- -
- -client.login('token here');
+import * as Discord from 'discord.js';
+import * as log from 'winston';
+import * as fs from 'fs';
+
+const client = new Discord.Client();
+const config = JSON.parse(fs.readFileSync('./config.json').toString()) as IConfig;
+
+interface IConfig {
+    token: string;
+}
+
+client.on('ready', () => {
+    log.info(`Client is ready, username: ${client.user.username}.`);
+});
+
+client.on('message', (msg) => {
+    if (msg.channel.type === 'dm') {
+        return;
+    }
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+client.login(config.token);
