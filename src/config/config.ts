efcbd65dfc184686ff97cacffab07676a1640957
Şamil NART <samil.nart@gmail.com>
2017-05-12 15:39:14 +0300

    Updated Config class to support db file name

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,16 @@
 interface IConfigFile {
     token: string;
+    dbFileName: string;
 }
 
 export class Config {
     public discordToken: string;
+    public dbFileName: string;
 
     constructor(private configPath: string) {
         let configData = require(this.configPath) as IConfigFile;
 
         this.discordToken = configData.token;
+        this.dbFileName = configData.dbFileName;
     }
 }
