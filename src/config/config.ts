6d93aa91f384ef37dabab3db3ac19cc88e3a705f
Şamil NART <samil.nart@gmail.com>
2017-05-15 21:32:51 +0300

    Added a default prefix entry to config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,16 +1,19 @@
 interface IConfigFile {
     token: string;
     dbFileName: string;
+    defaultPrefix: string;
 }
 
 export class Config {
     public discordToken: string;
     public dbFileName: string;
+    public defaultPrefix: string;
 
     constructor(private configPath: string) {
         let configData = require(this.configPath) as IConfigFile;
 
         this.discordToken = configData.token;
         this.dbFileName = configData.dbFileName;
+        this.defaultPrefix = configData.defaultPrefix;
     }
 }
