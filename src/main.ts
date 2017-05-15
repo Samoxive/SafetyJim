496f2df85d69ec6221a94ad8d346789339037ddc
Şamil NART <samil.nart@gmail.com>
2017-05-15 23:11:29 +0300

    Colorful logs!

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as Discord from 'discord.js';
-import * as log from 'winston';
+import * as winston from 'winston';
 import * as fs from 'fs';
 import * as path from 'path';
 import { BotDatabase } from './database/database';
@@ -7,8 +7,18 @@ import { Config } from './config/config';
 import { SafetyJim } from './safetyjim/safetyjim';
 
 const config = new Config(path.join(__dirname, '..', 'config.json'));
-const database = new BotDatabase(config);
+const log = new winston.Logger({
+    transports: [
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true,
+        }),
+    ],
+});
 
+const database = new BotDatabase(config, log);
 database.init().then((db) => {
-    const bot = new SafetyJim(config, db);
+    const bot = new SafetyJim(config, db, log);
 });
