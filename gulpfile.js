712f015e7dffdda505294d44bb3939e83f3f2a8d
Şamil NART <samil.nart@gmail.com>
2017-05-16 11:22:36 +0300

    Added run command

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,12 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
 const clean = require('gulp-clean');
+const exec = require('child_process').exec;
+
 const tsProject = ts.createProject('tsconfig.json');
 
 
-gulp.task('default', () => {
-    gulp.src('src/**/*.ts')
-        .pipe(sourcemaps.init())
-        .pipe(tsProject())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('src'));
-});
+gulp.task('default', ['build']);
 
 gulp.task('clean', () => {
     gulp.src('src/**/*.js', {read: false})
@@ -25,3 +21,18 @@ gulp.task('clean', () => {
 });
 
 gulp.task('clean-build', ['clean', 'default'], () => {});
+
+gulp.task('build', () => {
+    gulp.src('src/**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src'));
+});
+
+gulp.task('run', ['build'], () => {
+    let proc = exec('node .\\src\\main.js')
+
+    proc.stdout.pipe(process.stdout);
+    proc.stderr.pipe(process.stderr);
+})
