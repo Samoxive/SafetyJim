6328a42b07a613ad81828edea173fb8bdb7e2d51
Şamil NART <samil.nart@gmail.com>
2017-05-16 09:31:45 +0300

    Added gulpfile with build and clean commands

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
+const clean = require('gulp-clean');
+const tsProject = ts.createProject('tsconfig.json');
+
+
+gulp.task('default', () => {
+    gulp.src('src/**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src'));
+});
+
+gulp.task('clean', () => {
+    gulp.src('src/**/*.js', {read: false})
+        .pipe(clean());
+    
+    gulp.src('src/**/*.js.map', {read: false})
+        .pipe(clean());
+
+    gulp.src('jims_memory.db', {read: false})
+        .pipe(clean());
+});
