3c67d3f2244336e4256a867e8373a2fbd9ee5a6d
Heasummn <hassamuddin20@gmail.com>
2017-05-29 16:02:18 -0500

    check if correct config settings are provided or set defaults

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,6 @@
+let defaultConfigDbName = "database.db";
+let defaultConfigPrefix = "!"
+
 interface IConfigFile {
     token: string;
     dbFileName: string;
@@ -15,12 +18,27 @@ export class Config {
         try {
             configData = require(this.configPath) as IConfigFile;
         } catch(e) {
+            // Log with Winston?
             console.error("Loading config file failed with error: " + e.message);
             process.exit(e.code);
         }
-
+        
         this.discordToken = configData.token;
+        if(this.discordToken == undefined) {
+            console.error("Discord Token not provided!");
+            process.exit(1);
+        }
+
         this.dbFileName = configData.dbFileName;
+        if(this.dbFileName == undefined) {
+            console.error("Database file name not provided!\nUsing '" + defaultConfigDbName + "'  as default!");
+            this.dbFileName = defaultConfigDbName;
+        }
+
         this.defaultPrefix = configData.defaultPrefix;
+        if(this.defaultPrefix == undefined) {
+            console.error("Default prefix not provided!\nUsing '" + defaultConfigPrefix + "' as default!");
+            this.defaultPrefix = defaultConfigPrefix;
+        }
     }
 }
