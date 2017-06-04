875b7b03bc6a357ac307aec1a96d83628dddef46
Şamil NART <samil.nart@gmail.com>
2017-06-04 19:02:23 +0300

    Added mod log related commands

--- src/commands/settings/settings.ts
+++ src/commands/settings/settings.ts
@@ -6,11 +6,13 @@ class Settings implements Command {
     public usage = [
         'settings display - shows current state of settings',
         'settings prefix set <newPrefix> - sets a new prefix',
+        'settings modlog <enable/disable> - enables or disables mod log feature',
+        'settings modlog set channel <#channelName> - sets what channel mod logs are posted to',
         'settings holdingRoom <enable/disable> - enables or disables holding room feature',
         'settings holdingRoom set role <roleName> - sets the role assigned to users when holding time passes',
         // tslint:disable-next-line:max-line-length
         'settings holdingRoom set minutes <minutes> - sets how much minutes a new user has to wait before being allowed',
-        'settings holdingRoom set channel <#channel_name> - sets what channel welcome messages are posted to',
+        'settings holdingRoom set channel <#channelName> - sets what channel welcome messages are posted to',
     ];
 
     // tslint:disable-next-line:no-empty
@@ -18,12 +20,41 @@ class Settings implements Command {
 
     public run(bot: SafetyJim, msg: Discord.Message, args: string): boolean {
         let splitArgs = args.split(' ');
-        if (!args || !['display', 'holdingRoom', 'prefix'].includes(splitArgs[0])) {
+        if (!args || !['display', 'holdingRoom', 'prefix', 'modlog'].includes(splitArgs[0])) {
             return true;
         }
 
         if (splitArgs[0] === 'display') {
             this.handleSettingsDisplay(bot, msg);
+            return;
+        } else if (splitArgs[0] === 'modlog') {
+            if (!splitArgs[1] || !['enable', 'disable', 'set'].includes(splitArgs[1])) {
+                return true;
+            }
+
+            switch (splitArgs[1]) {
+                case 'enable':
+                case 'disable':
+                    this.handleModLogSwitch(bot, msg, splitArgs[1] === 'enable');
+                    break;
+                case 'set':
+                    if (splitArgs.length < 3 || splitArgs[2] !== 'channel') {
+                        return true;
+                    }
+
+                    if (msg.mentions.channels.size === 0 ||
+                        !Discord.MessageMentions.CHANNELS_PATTERN.test(splitArgs[3])) {
+                        msg.channel.send('Invalid channel input, no changes were made!');
+                        return;
+                    }
+
+                    let channel = msg.mentions.channels.first();
+
+                    bot.log.info(`Updated channel for mod log in guild "${msg.guild}" with id: "${msg.guild.id}".`);
+                    bot.database.updateGuildConfig(msg.guild, { modLogChannelID: channel.id });
+                    break;
+            }
+
             return;
         } else if (splitArgs[0] === 'holdingRoom') {
             if (!splitArgs[1] || !['enable', 'disable', 'set'].includes(splitArgs[1])) {
@@ -61,6 +92,13 @@ class Settings implements Command {
         output += `Prefix: ${prefix}\n`;
         output += `Embed color: ${config.EmbedColor}\n`;
 
+        if (config.ModLogActive === 0) {
+            output += 'Mod Log: Disabled\n';
+        } else {
+            output += 'Mod Log: Enabled\n';
+            output += `\tMod Log Channel: ${msg.guild.channels.get(config.ModLogChannelID).name}\n`;
+        }
+
         if (config.HoldingRoomActive === 0) {
             output += 'Holding Room: Disabled\n';
         } else {
@@ -81,6 +119,29 @@ class Settings implements Command {
                 msg.channel.send('There was an error while trying to display settings, this incident has been logged.');
                 bot.log.error(`Could not display settings for guild: "${msg.guild.name}" with id: "${msg.guild.id}"`);
             });
+    }
+
+    private async handleModLogSwitch(bot: SafetyJim, msg: Discord.Message, enable: boolean): Promise<void> {
+        let config = await bot.database.getGuildConfiguration(msg.guild);
+
+        if (!enable) {
+            if (config.ModLogActive === 0) {
+                msg.channel.send('Mod log is already disabled silly.');
+            } else {
+                msg.channel.send('Disabled mod log.');
+                bot.log.info(`Disabled mod log for guild: "${msg.guild}" with id: "${msg.guild.id}".`);
+                bot.database.updateGuildConfig(msg.guild, { modLog: false });
+                return;
+            }
+        } else {
+            if (config.ModLogActive === 1) {
+            msg.channel.send('Mod log is already enabled silly.');
+            } else {
+                bot.database.updateGuildConfig(msg.guild, { modLog: true });
+                bot.log.info(`Enabled mod log for guild: "${msg.guild}" with id: "${msg.guild.id}".`);
+                msg.channel.send('Enabled mod log.');
+            }
+        }
     }
 
     private async handleHoldingRoomSwitch(bot: SafetyJim, msg: Discord.Message, enable: boolean): Promise<void> {
