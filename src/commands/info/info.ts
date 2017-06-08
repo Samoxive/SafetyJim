8a5d69a69ac949bf05ce721a14b3fae1a2c5ce8a
Şamil NART <samil.nart@gmail.com>
2017-06-08 17:27:32 +0300

    Updated uptime method

--- src/commands/info/info.ts
+++ src/commands/info/info.ts
@@ -4,6 +4,14 @@ import * as Discord from 'discord.js';
 class Info implements Command {
     public usage = 'info - displays some information about the bot';
 
+    private MAGNITUDES = [
+        [1000 * 60 * 60 * 24 * 30, 'months'],
+        [1000 * 60 * 60 * 24, 'days'],
+        [1000 * 60 * 60, 'hours'],
+        [1000 * 60, 'minutes'],
+        [1000, 'seconds'],
+    ];
+
     // tslint:disable-next-line:no-empty
     constructor(bot: SafetyJim) {}
 
@@ -14,53 +22,36 @@ class Info implements Command {
 
     private async asyncRun(bot: SafetyJim, msg: Discord.Message, args: string): Promise<void> {
         let config = await bot.database.getGuildConfiguration(msg.guild);
-        let uptimeString = this.millisecondsToStr((new Date()).getTime() - bot.bootTime.getTime());
+        let uptimeString = this.timeElapsed((new Date()).getTime(), bot.bootTime.getTime());
         let embed = new Discord.RichEmbed({
             author: { name: 'Safety Jim - v0.0.1', icon_url: bot.client.user.avatarURL },
             description: `Lifting the :hammer: since ${uptimeString} ago.`,
             fields: [
-                { name: 'Guild Count', value:  bot.client.guilds.size.toString(), inline: true },
+                { name: 'Server Count', value:  bot.client.guilds.size.toString(), inline: true },
                 { name: 'User Count', value: bot.client.users.size.toString(), inline: true },
-                { name: 'Channel Count', value: bot.client.channels.size.toString(), inline: true },
+                { name: 'Github', value: '[Samoxive/SafetyJim](https://github.com/samoxive/safetyjim)', inline: true },
                 { name: 'Websocket Ping', value: `${bot.client.ping.toFixed(0)}ms`, inline: true},
                 // tslint:disable-next-line:max-line-length
                 { name: 'RAM usage', value: `${(process.memoryUsage().rss / (1024 * 1024)).toFixed(0)}MB`, inline: true },
-                { name: 'Support', value: '[discord.io/safetyjim](https://discord.io/safetyjim)', inline: true },
+                { name: 'Support', value: '[discord.io/SafetyJim](https://discord.io/safetyjim)', inline: true },
             ],
-            footer: { text: `Made by Samoxive#8634 and Aetheryx#2222.`},
+            footer: { text: `Made by Safety Jim team.`},
             color: parseInt(config.EmbedColor, 16),
         });
 
         msg.channel.send('', {embed});
     }
 
-    private millisecondsToStr(milliseconds: number): string {
-        function numberEnding(n: number) {
-            return (n > 1) ? 's' : '';
-        }
-
-        let temp = Math.floor(milliseconds / 1000);
-        let years = Math.floor(temp / 31536000);
-        if (years) {
-            return years + ' year' + numberEnding(years);
-        }
-        let days = Math.floor((temp %= 31536000) / 86400);
-        if (days) {
-            return days + ' day' + numberEnding(days);
-        }
-        let hours = Math.floor((temp %= 86400) / 3600);
-        if (hours) {
-            return hours + ' hour' + numberEnding(hours);
-        }
-        let minutes = Math.floor((temp %= 3600) / 60);
-        if (minutes) {
-            return minutes + ' minute' + numberEnding(minutes);
-        }
-        let seconds = temp % 60;
-        if (seconds) {
-            return seconds + ' second' + numberEnding(seconds);
-        }
-        return 'less than a second';
+    private timeElapsed(before: number, after: number) {
+        let diff = Math.abs(after - before);
+        return this.MAGNITUDES.reduce((out, m: [number, string]) => {
+          const current = Math.floor(diff / m[0]);
+          diff %= m[0];
+          if (out.length || current) {
+            out.push(`${current} ${m[1]}`);
+          }
+          return out;
+        }, []).join(' and ');
     }
 }
 
