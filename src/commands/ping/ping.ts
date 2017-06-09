95918e723ffac0fb5d94975eae47fe3f2b2d1e4b
Şamil NART <samil.nart@gmail.com>
2017-06-09 17:35:57 +0300

    ping now sends an embed

--- src/commands/ping/ping.ts
+++ src/commands/ping/ping.ts
@@ -2,13 +2,22 @@ import { Command, SafetyJim } from '../../safetyjim/safetyjim';
 import * as Discord from 'discord.js';
 
 class Ping implements Command {
-    public usage = 'ping - pongs when you ping doofus';
+    public usage = 'ping - pong';
 
     // tslint:disable-next-line:no-empty
     constructor(bot: SafetyJim) {}
 
     public run(bot: SafetyJim, msg: Discord.Message, args: string): boolean {
-        msg.channel.send('pong');
+        bot.successReact(msg);
+        msg.channel.send('', { embed: {
+            author: {
+                name: `Safety Jim`,
+                icon_url: bot.client.user.avatarURL,
+            },
+            description: `:ping_pong: Ping: ${bot.client.ping.toFixed(0)}`,
+            color: 0x4286f4,
+        }});
+
         return;
     }
 }
