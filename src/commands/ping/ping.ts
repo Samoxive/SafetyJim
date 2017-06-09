72925f7da3e4589c39024314d2507b58994f4e66
Şamil NART <samil.nart@gmail.com>
2017-06-09 17:43:11 +0300

    Add ms

--- src/commands/ping/ping.ts
+++ src/commands/ping/ping.ts
@@ -14,7 +14,7 @@ class Ping implements Command {
                 name: `Safety Jim`,
                 icon_url: bot.client.user.avatarURL,
             },
-            description: `:ping_pong: Ping: ${bot.client.ping.toFixed(0)}`,
+            description: `:ping_pong: Ping: ${bot.client.ping.toFixed(0)}ms`,
             color: 0x4286f4,
         }});
 
