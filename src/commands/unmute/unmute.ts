15d3c5cadc07d7a1cabf119bf1fef4c2bc13e160
Şamil NART <samil.nart@gmail.com>
2017-06-17 16:21:28 +0300

    Added unmute command

---
+++ src/commands/unmute/unmute.ts
@@ -0,0 +1,46 @@
+import { Command, SafetyJim } from '../../safetyjim/safetyjim';
+import * as Discord from 'discord.js';
+
+class Unmute implements Command {
+    public usage = 'ping - pong';
+
+    // tslint:disable-next-line:no-empty
+    constructor(bot: SafetyJim) {}
+
+    public run(bot: SafetyJim, msg: Discord.Message, args: string): boolean {
+        let splitArgs = args.split(' ');
+
+        if (!args) {
+            return true;
+        }
+
+        if (msg.mentions.users.size === 0 ||
+            !splitArgs[0].match(Discord.MessageMentions.USERS_PATTERN)) {
+            return true;
+        }
+
+        let role = msg.guild.roles.find('name', 'Muted');
+
+        if (!role) {
+            bot.failReact(msg);
+            // tslint:disable-next-line:max-line-length
+            msg.channel.send('Could not find a Muted role, please create one yourself or mute a user to automatically setup one.');
+            return;
+        }
+
+        let member = msg.mentions.members.first();
+
+        member.removeRole(role)
+              .then(() => {
+                  bot.database.updateMuteRecordWithID(member.user.id, msg.guild.id);
+                  bot.successReact(msg);
+                  msg.channel.send(`Unmuted user ${member}.`);
+              })
+              .catch(() => {
+                  bot.failReact(msg);
+                  msg.channel.send('Could not unmute specified user!');
+              });
+    }
+}
+
+export = Unmute;
