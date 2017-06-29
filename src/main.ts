d39d753f1bec46a51fbf44b9512000baf5dd2b28
Şamil NART <samil.nart@gmail.com>
2017-06-29 17:42:31 +0300

    Added a temporary solution to get more info from uncaught exceptions

--- src/main.ts
+++ src/main.ts
@@ -32,3 +32,17 @@ const database = new BotDatabase(config, log);
 database.init().then((db) => {
     const bot = new SafetyJim(config, db, log);
 });
+
+process.addListener('uncaughtException', (err) => {
+    log.error(`Uncaught Exception: ${err.message} : ${err.stack}`);
+});
+
+process.addListener('unhandledRejection', (err) => {
+    let isErr = (err.stack == null) || (err.message == null);
+
+    if (isErr) {
+        log.error(`Uncaught Rejection: ${err.message} : ${err.stack}`);
+    } else {
+        log.error(`Uncaught Rejection: ${JSON.stringify(err)}`);
+    }
+});
