fec373d6c72a8e35b16234122cbac927c67d029b
Şamil NART <samil.nart@gmail.com>
2017-07-02 14:52:21 +0300

    Added more checks

--- src/commands/clean/clean.ts
+++ src/commands/clean/clean.ts
@@ -11,11 +11,22 @@ class Clean implements Command {
 
     public run(bot: SafetyJim, msg: Discord.Message, args: string): boolean {
         let newArgs = args.split(' ');
-        let deleteAmount;
-        if (!parseInt(newArgs[0])) {
-            deleteAmount = 2;
-        } else {
-            deleteAmount = parseInt(newArgs[0]) + 1;
+        let deleteAmount = parseInt(newArgs[0]);
+
+        if (newArgs[0] === '' && newArgs.length === 1) {
+            bot.successReact(msg);
+            msg.channel.bulkDelete(2);
+            return;
+        }
+
+        if (isNaN(deleteAmount)) {
+            return true;
+        }
+
+        if (deleteAmount < 1) {
+            msg.channel.send('You can\'t delete zero or negative messages.');
+            bot.failReact(msg);
+            return;
         }
 
         if (deleteAmount > 100) {
@@ -26,29 +37,48 @@ class Clean implements Command {
 
         if (!newArgs[1]) {
             bot.successReact(msg);
-            msg.channel.bulkDelete(deleteAmount);
+            msg.channel.bulkDelete(deleteAmount + 1);
             return;
         }
 
-        if (!newArgs[1].match(Discord.MessageMentions.USERS_PATTERN) ||
+        if (!newArgs[1].match(Discord.MessageMentions.USERS_PATTERN) &&
             newArgs[1].toLowerCase() !== 'bot') {
                 bot.failReact(msg);
                 return true;
         }
 
         if (newArgs[1].match(Discord.MessageMentions.USERS_PATTERN)) {
+            bot.successReact(msg);
+            let deleteUser = msg.mentions.users.first();
+
+            if (deleteUser.id === msg.author.id) {
+                deleteAmount++;
+            }
+
             msg.channel.fetchMessages({ limit: 100 }).then((messages) => {
                 const newMessages = messages.filterArray((m) => m.author.id === msg.mentions.users.first().id)
                     .slice(0, deleteAmount);
-                msg.channel.bulkDelete(newMessages);
+
+                if (deleteAmount === 1) {
+                    newMessages[0].delete();
+                } else {
+                    msg.channel.bulkDelete(newMessages);
+                }
             });
         }
 
         if (newArgs[1].toLowerCase() === 'bot') {
+            bot.successReact(msg);
+
             msg.channel.fetchMessages({ limit: 100 }).then((messages) => {
                 const newMessages = messages.filterArray((m) => m.author.bot)
                     .slice(0, deleteAmount);
-                msg.channel.bulkDelete(newMessages);
+
+                if (deleteAmount === 1) {
+                    newMessages[0].delete();
+                } else {
+                    msg.channel.bulkDelete(newMessages);
+                }
             });
         }
         return;
