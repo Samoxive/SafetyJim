bb88de280fd4f89c5677db0859baaa9ee7e76d5c
Şamil NART <samil.nart@gmail.com>
2017-07-10 22:04:43 +0300

    Finalized migration script

--- scripts/db2.1_to_2.2.py
+++ scripts/db2.1_to_2.2.py
@@ -6,8 +6,22 @@ if len(sys.argv) < 2:
     sys.exit(-1)
 
 database = sqlite3.connect(sys.argv[1])
+database.row_factory = sqlite3.Row
 cursor = database.cursor()
 
+cursor.execute('CREATE TABLE IF NOT EXISTS Settings (GuildID TEXT NOT NULL, Key TEXT NOT NULL, Value TEXT);')
 cursor.execute('SELECT * FROM GuildSettings;')
 results = cursor.fetchall()
-print(results)
+
+for row in results:
+    guildID = row[0]
+    for pair in zip(row.keys(), row):
+        if pair[0] == 'GuildID':
+            continue
+
+        key, value = pair
+        values = (guildID, key, value,)
+        cursor.execute('INSERT INTO Settings (GuildID, Key, Value) VALUES (?, ?, ?);', values)
+
+database.commit()
+database.close()
