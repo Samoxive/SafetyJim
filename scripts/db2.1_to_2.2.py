5e07b25c897565fcb9549dae8e8a45ca60cea993
Şamil NART <samil.nart@gmail.com>
2017-07-11 12:45:28 +0300

    Added missing columns and int and boolean values are now converted to string

--- scripts/db2.1_to_2.2.py
+++ scripts/db2.1_to_2.2.py
@@ -13,15 +13,44 @@ cursor.execute('CREATE TABLE IF NOT EXISTS Settings (GuildID TEXT NOT NULL, Key 
 cursor.execute('SELECT * FROM GuildSettings;')
 results = cursor.fetchall()
 
+print('GuildSettings row count: {}'.format(len(results)))
 for row in results:
     guildID = row[0]
     for pair in zip(row.keys(), row):
         if pair[0] == 'GuildID':
             continue
 
+        if (pair[0] == 'ModLogActive') or (pair[0] == 'HoldingRoomActive'):
+            pair = (pair[0], 'true' if pair[1] == '1' else 'false')
+        
+        if pair[0] == 'HoldingRoomMinutes':
+            pair = (pair[0], str(pair[1]))
+
         key, value = pair
         values = (guildID, key, value,)
         cursor.execute('INSERT INTO Settings (GuildID, Key, Value) VALUES (?, ?, ?);', values)
+
+cursor.execute('SELECT * FROM PrefixList;')
+results = cursor.fetchall()
+
+print('PrefixList row count: {}'.format(len(results)))
+for row in results:
+    guildID = row[0]
+    prefix = row[1]
+
+    values = (guildID, 'Prefix', prefix,)
+    cursor.execute('INSERT INTO Settings (GuildID, Key, Value) VALUES (?, ?, ?);', values)
+
+cursor.execute('SELECT * FROM WelcomeMessages;')
+results = cursor.fetchall()
+
+print('WelcomeMessages row count: {}'.format(len(results)))
+for row in results:
+    guildID = row[0]
+    message = row[1]
+
+    values = (guildID, 'WelcomeMessage', message,)
+    cursor.execute('INSERT INTO Settings (GuildID, Key, Value) VALUES (?, ?, ?);', values)
 
 database.commit()
 database.close()
