457989837caceabfcb186fd103f7c3682e741469
Şamil NART <samil.nart@gmail.com>
2017-09-22 20:54:09 +0300

    Added shard id and count to the embed

--- src/commands/ping/ping.ts
+++ src/commands/ping/ping.ts
@@ -1,5 +1,7 @@
 import { Command, SafetyJim } from '../../safetyjim/safetyjim';
+import { Shard } from '../../safetyjim/shard';
 import * as Discord from 'discord.js';
+import * as Utils from '../../safetyjim/utils';
 
 class Ping implements Command {
     public usage = 'ping - pong';
@@ -7,14 +9,14 @@ class Ping implements Command {
     // tslint:disable-next-line:no-empty
     constructor(bot: SafetyJim) {}
 
-    public async run(bot: SafetyJim, msg: Discord.Message, args: string): Promise<boolean> {
-        await bot.successReact(msg);
-        await bot.sendMessage(msg.channel, { embed: {
+    public async run(shard: Shard, jim: SafetyJim, msg: Discord.Message, args: string): Promise<boolean> {
+        await shard.successReact(msg);
+        await Utils.sendMessage(msg.channel, { embed: {
             author: {
-                name: `Safety Jim`,
-                icon_url: bot.client.user.avatarURL,
+                name: `Safety Jim [${shard.shardId + 1}, ${jim.config.jim.shard_count}]`,
+                icon_url: shard.client.user.avatarURL,
             },
-            description: `:ping_pong: Ping: ${bot.client.pings[0].toFixed(0)}ms`,
+            description: `:ping_pong: Ping: ${shard.client.pings[0].toFixed(0)}ms`,
             color: 0x4286f4,
         }});
 
