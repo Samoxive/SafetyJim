065e3412ca07aa15f96aab35445c52629778c2a6
Şamil NART <samil.nart@gmail.com>
2017-09-30 17:32:49 +0300

    Declared Commands type

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate serenity;
 use std::string::String;
+use std::sync::Arc;
+use std::collections::HashMap;
 pub mod config;
+pub mod commands;
 
 pub trait Command {
-    fn usage() -> Vec<String> {
+    fn usage(&self) -> Vec<String> {
         vec!(String::from("command - default usage text"))
     }
 }
+
+pub type Commands = HashMap<String, Arc<Command>>;
 
 
 #[cfg(test)]
