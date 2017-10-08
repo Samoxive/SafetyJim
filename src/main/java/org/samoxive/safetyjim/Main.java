a82823a7749d74a37166fc8ecf275e49611b046f
Şamil NART <samil.nart@gmail.com>
2017-10-08 17:23:43 +0300

    Removed jooq experiement

--- src/main/java/org/samoxive/safetyjim/Main.java
+++ src/main/java/org/samoxive/safetyjim/Main.java
@@ -6,24 +6,13 @@ import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 import org.samoxive.jooq.generated.Tables;
 import org.samoxive.jooq.generated.tables.records.*;
+import org.samoxive.safetyjim.config.Config;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Main {
     public static void main(String ...args) {
-        String userName = "postgres";
-        String password = "postgres";
-        String url = "jdbc:postgresql://localhost:5432/dev";
 
-        try {
-            Connection conn = DriverManager.getConnection(url, userName, password);
-            DSLContext create = DSL.using(conn, SQLDialect.POSTGRES);
-            BanlistRecord record = create.newRecord(Tables.BANLIST);
-            record.setExpires(true);
-            record.store();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
