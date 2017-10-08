582e1114388ecf2307755d49a8a42527b9ed6a99
Şamil NART <samil.nart@gmail.com>
2017-10-08 16:03:17 +0300

    Changed how models are important according to the jooq code generation changes

--- src/main/java/org/samoxive/safetyjim/Main.java
+++ src/main/java/org/samoxive/safetyjim/Main.java
@@ -1,10 +1,11 @@
 package org.samoxive.safetyjim;
 
-import jooq.generated.Tables;
-import jooq.generated.tables.records.BanlistRecord;
+
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
+import org.samoxive.jooq.generated.Tables;
+import org.samoxive.jooq.generated.tables.records.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,7 +16,8 @@ public class Main {
         String password = "postgres";
         String url = "jdbc:postgresql://localhost:5432/dev";
 
-        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+        try {
+            Connection conn = DriverManager.getConnection(url, userName, password);
             DSLContext create = DSL.using(conn, SQLDialect.POSTGRES);
             BanlistRecord record = create.newRecord(Tables.BANLIST);
             record.setExpires(true);
