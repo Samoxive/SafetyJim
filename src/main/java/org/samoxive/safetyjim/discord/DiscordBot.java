d55b21bc6eca5b62d0f557efd4a21c8a8b85767d
Şamil NART <samil.nart@gmail.com>
2017-10-10 23:06:52 +0300

    Added shard generation

--- src/main/java/org/samoxive/safetyjim/discord/DiscordBot.java
+++ src/main/java/org/samoxive/safetyjim/discord/DiscordBot.java
@@ -1,14 +1,39 @@
 package org.samoxive.safetyjim.discord;
 
 import org.jooq.DSLContext;
+import org.samoxive.safetyjim.config.Config;
 
 import java.util.List;
 
 public class DiscordBot {
     private List<DiscordShard> shards;
     private DSLContext database;
+    private Config config;
 
-    public DiscordBot(DSLContext database) {
+    public DiscordBot(DSLContext database, Config config) {
         this.database = database;
+        this.config = config;
+
+        for (int i = 0; i < config.jim.shard_count; i++) {
+            DiscordShard shard = new DiscordShard(this, i);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public List<DiscordShard> getShards() {
+        return shards;
+    }
+
+    public DSLContext getDatabase() {
+        return database;
+    }
+
+    public Config getConfig() {
+        return config;
     }
 }
