a21e771f069d99fa29c69aa07fc975187c07fcfc
Şamil NART <samil.nart@gmail.com>
2017-10-11 21:57:14 +0300

    Added command and processor collections

--- src/main/java/org/samoxive/safetyjim/discord/DiscordBot.java
+++ src/main/java/org/samoxive/safetyjim/discord/DiscordBot.java
@@ -5,17 +5,22 @@ import org.jooq.DSLContext;
 import org.samoxive.safetyjim.config.Config;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class DiscordBot {
     private List<DiscordShard> shards;
     private DSLContext database;
     private Config config;
+    private HashMap<String, Command> commands;
+    private HashMap<String, MessageProcessor> processors;
 
     public DiscordBot(DSLContext database, Config config) {
         this.database = database;
         this.config = config;
         this.shards = new ArrayList<>();
+        this.commands = new HashMap<>();
+        this.processors = new HashMap<>();
 
         for (int i = 0; i < config.jim.shard_count; i++) {
             DiscordShard shard = new DiscordShard(this, i);
@@ -34,6 +39,10 @@ public class DiscordBot {
                      .map(shard -> shard.getShard())
                      .mapToLong(shards -> shards.getGuildCache().size())
                      .sum();
+    }
+
+    private void loadCommands() {
+
     }
 
     public List<DiscordShard> getShards() {
