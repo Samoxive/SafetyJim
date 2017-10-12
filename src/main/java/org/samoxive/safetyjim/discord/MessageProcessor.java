5e791d31686779e9ea55a4a97635a73c8e624bcc
Şamil NART <samil.nart@gmail.com>
2017-10-12 16:17:41 +0300

    Fixed wrong parameter types

--- src/main/java/org/samoxive/safetyjim/discord/MessageProcessor.java
+++ src/main/java/org/samoxive/safetyjim/discord/MessageProcessor.java
@@ -1,14 +1,16 @@
 package org.samoxive.safetyjim.discord;
 
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageDeleteEvent;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.guild.react.GuildMessageReactionAddEvent;
 import net.dv8tion.jda.core.events.message.guild.react.GuildMessageReactionRemoveEvent;
 
 public abstract class MessageProcessor {
     public boolean onMessage(DiscordBot bot, GuildMessageReceivedEvent event) {
         return false;
     }
-    public void onMessageDelete(DiscordBot bot, GuildMessageReactionRemoveEvent event) {}
-    public void onReactionAdd(DiscordBot bot, GuildMessageReceivedEvent event) {}
+    public void onMessageDelete(DiscordBot bot, GuildMessageDeleteEvent event) {}
+    public void onReactionAdd(DiscordBot bot, GuildMessageReactionAddEvent event) {}
     public void onReactionRemove(DiscordBot bot, GuildMessageReactionRemoveEvent event) {}
 }
