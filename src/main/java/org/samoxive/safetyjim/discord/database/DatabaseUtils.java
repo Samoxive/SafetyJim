267d6abe4da22100cd4f59e36cc267d47e8ec07b
Şamil NART <samil.nart@gmail.com>
2017-10-12 16:17:53 +0300

    Added guild setting related methods

--- src/main/java/org/samoxive/safetyjim/discord/database/DatabaseUtils.java
+++ src/main/java/org/samoxive/safetyjim/discord/database/DatabaseUtils.java
@@ -3,12 +3,46 @@ package org.samoxive.safetyjim.discord.database;
 import net.dv8tion.jda.core.entities.Guild;
 import org.jooq.DSLContext;
 import org.samoxive.jooq.generated.Tables;
+import org.samoxive.jooq.generated.tables.Settings;
 import org.samoxive.jooq.generated.tables.records.SettingsRecord;
 import org.samoxive.safetyjim.discord.DiscordBot;
 import org.samoxive.safetyjim.discord.DiscordUtils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DatabaseUtils {
+    public static final String[] possibleSettingKeys = {
+            "modlogactive",
+            "modlogchannelid",
+            "holdingroomroleid",
+            "holdingroomactive",
+            "invitelinkremover",
+            "holdingroomminutes",
+            "welcomemessagechannelid",
+            "prefix",
+            "welcomemessage",
+            "welcomemessageactive",
+            "silentcommands"
+    };
     private static final String DEFAULT_WELCOME_MESSAGE = "Welcome to $guild $user!";
+
+    public static Map<String, String> getGuildSettings(DSLContext database, Guild guild) {
+        Map<String, String> settings = new HashMap<>();
+        SettingsRecord[] settingsRecords = database.selectFrom(Tables.SETTINGS)
+                                                   .where(Tables.SETTINGS.GUILDID.eq(guild.getId()))
+                                                   .fetchArray();
+        for (SettingsRecord record: settingsRecords) {
+            settings.put(record.getKey(), record.getValue());
+        }
+
+        return settings;
+    }
+
+    public static void deleteGuildSettings(DSLContext database, Guild guild) {
+        database.deleteFrom(Tables.SETTINGS).where(Tables.SETTINGS.GUILDID.eq(guild.getId())).execute();
+    }
+
     public static void createGuildSettings(DiscordBot bot, DSLContext database, Guild guild) {
         createKeyValueSetting(database, guild, "silentcommands", "false");
         createKeyValueSetting(database, guild, "invitelinkremover", "false");
