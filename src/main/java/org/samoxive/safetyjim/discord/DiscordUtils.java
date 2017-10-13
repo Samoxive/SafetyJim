902e7576d6ead07708059e75d648f099fdd0544a
Şamil NART <samil.nart@gmail.com>
2017-10-13 22:58:11 +0300

    Added success and fail reaction methods

--- src/main/java/org/samoxive/safetyjim/discord/DiscordUtils.java
+++ src/main/java/org/samoxive/safetyjim/discord/DiscordUtils.java
@@ -1,15 +1,24 @@
 package org.samoxive.safetyjim.discord;
 
-import net.dv8tion.jda.core.entities.Guild;
-import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class DiscordUtils {
+    private static final Logger log = LoggerFactory.getLogger(DiscordUtils.class);
     private static final String[] botListIds = {"110373943822540800", // DiscordBots
                                                 "264445053596991498", // DiscordBotList
                                                 "297462937646530562", // NovoBotList
                                                };
+    private static String SUCCESS_EMOTE_ID = "322698554294534144";
+    private static String FAIL_EMOTE_ID = "322698553980092417";
+    private static Emote SUCCESS_EMOTE;
+    private static Emote FAIL_EMOTE;
+
     public static boolean isBotFarm(Guild guild) {
         for (String botList: botListIds) {
             if (guild.getId().equals(botList)) {
@@ -22,12 +31,70 @@ public class DiscordUtils {
 
     }
 
+    public static void successReact(DiscordBot bot, Message message) {
+        if (SUCCESS_EMOTE == null) {
+            for (DiscordShard shard: bot.getShards()) {
+                Emote emote = shard.getShard().getEmoteById(SUCCESS_EMOTE_ID);
+                if (emote != null) {
+                    SUCCESS_EMOTE = emote;
+                }
+            }
+
+            if (SUCCESS_EMOTE == null) {
+                log.error("Success emote couldn't be found. Aborting...");
+                System.exit(1);
+            }
+        }
+
+        reactToMessage(message, SUCCESS_EMOTE);
+    }
+
+    public static void failReact(DiscordBot bot, Message message) {
+        if (FAIL_EMOTE == null) {
+            for (DiscordShard shard: bot.getShards()) {
+                Emote emote = shard.getShard().getEmoteById(FAIL_EMOTE_ID);
+                if (emote != null) {
+                    FAIL_EMOTE = emote;
+                }
+            }
+
+            if (FAIL_EMOTE == null) {
+                log.error("Failure emote couldn't be found. Aborting...");
+                System.exit(1);
+            }
+        }
+
+        reactToMessage(message, FAIL_EMOTE);
+    }
+
+    public static void reactToMessage(Message message, Emote emote) {
+        try {
+            message.addReaction(emote).queue();
+        } catch (Exception e) {
+            //
+        }
+    }
+
     public static void sendMessage(TextChannel channel, String message) {
         try {
             channel.sendMessage(message).queue();
         } catch (Exception e) {
             //
         }
+    }
+
+    public static String getUsageString(String prefix, String[] usages) {
+        StringJoiner joiner = new StringJoiner("\n");
+
+        Arrays.stream(usages).map((usage) -> usage.split(" - "))
+                             .map((splitUsage) -> String.format("`%s %s` - %s", prefix, splitUsage[0], splitUsage[1]))
+                             .forEach((usage) -> joiner.add(usage));
+
+        return joiner.toString();
+    }
+
+    public static String getTag(User user) {
+        return user.getName() + "#" + user.getDiscriminator();
     }
 
     public static TextChannel getDefaultChannel(Guild guild) {
