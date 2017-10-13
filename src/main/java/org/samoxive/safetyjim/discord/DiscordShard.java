084ff4f430dde1ac6ed03b5f0467f13433045b2c
Şamil NART <samil.nart@gmail.com>
2017-10-13 22:58:01 +0300

    Added command execution and onMessage events

--- src/main/java/org/samoxive/safetyjim/discord/DiscordShard.java
+++ src/main/java/org/samoxive/safetyjim/discord/DiscordShard.java
@@ -1,11 +1,10 @@
 package org.samoxive.safetyjim.discord;
 
 import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.ChannelType;
-import net.dv8tion.jda.core.entities.Game;
-import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.*;
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.core.events.guild.GuildLeaveEvent;
@@ -19,18 +18,26 @@ import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.requests.SessionReconnectQueue;
 import org.jooq.DSLContext;
+import org.samoxive.jooq.generated.Tables;
+import org.samoxive.jooq.generated.tables.records.CommandlogsRecord;
 import org.samoxive.safetyjim.database.DatabaseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
-import java.util.Map;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class DiscordShard extends ListenerAdapter {
     private Logger log;
     private DiscordBot bot;
     private JDA shard;
     private int shardId;
+    private ExecutorService threadPool;
 
     public DiscordShard(DiscordBot bot, int shardId) {
         this.bot = bot;
@@ -54,6 +61,8 @@ public class DiscordShard extends ListenerAdapter {
         } catch (RateLimitedException e) {
             System.out.println("Hit Discord API Rate Limit");
         }
+
+        threadPool = Executors.newCachedThreadPool();
     }
 
     @Override
@@ -86,29 +95,77 @@ public class DiscordShard extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        if (event.getMessage().getContent().equals("ping")) {
-            event.getChannel().sendMessage("Pong, Guild Count" + bot.getGuildCount()).complete();
+        if (event.getAuthor().isBot()) {
+            return;
         }
-    }
 
-    @Override
-    public void onResume(ResumedEvent event) {
-        super.onResume(event);
-    }
+        DSLContext database = bot.getDatabase();
+        Message message = event.getMessage();
 
-    @Override
-    public void onReconnect(ReconnectedEvent event) {
-        super.onReconnect(event);
-    }
+        if (message.isMentioned(shard.getSelfUser()) && message.getContent().contains("prefix")) {
+            String prefix = database.selectFrom(Tables.SETTINGS)
+                                    .where(Tables.SETTINGS.GUILDID.eq(message.getGuild().getId()))
+                                    .and(Tables.SETTINGS.KEY.eq("prefix"))
+                                    .fetchOne()
+                                    .getValue();
+            DiscordUtils.successReact(bot, message);
 
-    @Override
-    public void onDisconnect(DisconnectEvent event) {
-        super.onDisconnect(event);
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setAuthor("Safety Jim - Prefix", null, shard.getSelfUser().getAvatarUrl())
+                 .setDescription("This guild's prefix is: " + prefix)
+                 .setColor(new Color(0x4286F4));
+
+            message.getChannel().sendMessage(embed.build()).queue();
+            return;
+        }
+
+        List<Future<Boolean>> processorResults = new LinkedList<>();
+        for (MessageProcessor processor: bot.getProcessors().values()) {
+            Future<Boolean> future = threadPool.submit(() -> processor.onMessage(bot, event));
+            processorResults.add(future);
+        }
+
+        for (Future<Boolean> result: processorResults) {
+            try {
+                if (result.get().equals(true)) {
+                    return;
+                }
+            } catch (Exception e) {
+                //
+            }
+        }
+
+        String prefix = DatabaseUtils.getGuildSetting(bot.getDatabase(), event.getGuild(), "prefix");
+
+        String[] splitContent = message.getContent().trim().split(" ");
+
+        if (!splitContent[0].equals(prefix)) {
+            return;
+        }
+
+        if (splitContent.length == 1) {
+            DiscordUtils.failReact(bot, message);
+            return;
+        }
+
+        Command command = bot.getCommands().get(splitContent[1]);
+
+        if (command == null) {
+            DiscordUtils.failReact(bot, message);
+            return;
+        }
+        StringJoiner args = new StringJoiner(" ");
+
+        for (int i = 2; i < splitContent.length; i++) {
+            args.add(splitContent[i]);
+        }
+
+        threadPool.execute(() -> executeCommand(event, command, splitContent[1], args.toString()));
     }
 
     @Override
     public void onException(ExceptionEvent event) {
-        super.onException(event);
+        log.error(event.toString());
     }
 
     @Override
@@ -143,16 +200,19 @@ public class DiscordShard extends ListenerAdapter {
     public void onGuildJoin(GuildJoinEvent event) {
         if (DiscordUtils.isBotFarm(event.getGuild())) {
             event.getGuild().leave().queue();
+            return;
         }
 
         String message = String.format("Hello! I am Safety Jim, `%s` is my default prefix!", bot.getConfig().jim.default_prefix);
         DiscordUtils.sendMessage(DiscordUtils.getDefaultChannel(event.getGuild()), message);
         DatabaseUtils.createGuildSettings(bot, bot.getDatabase(), event.getGuild());
+        bot.getMetrics().increment("guild.join");
     }
 
     @Override
     public void onGuildLeave(GuildLeaveEvent event) {
-        super.onGuildLeave(event);
+        bot.getDatabase().deleteFrom(Tables.SETTINGS).where(Tables.SETTINGS.GUILDID.eq(event.getGuild().getId()));
+        bot.getMetrics().increment("guild.left");
     }
 
     @Override
@@ -167,5 +227,48 @@ public class DiscordShard extends ListenerAdapter {
 
     public JDA getShard() {
         return shard;
+    }
+
+    private void createCommandLog(GuildMessageReceivedEvent event, Command command, String commandName, String args) {
+        User author = event.getAuthor();
+        CommandlogsRecord record = Tables.COMMANDLOGS.newRecord();
+        record.setCommand(commandName);
+        record.setArguments(args);
+        record.setUsername(DiscordUtils.getTag(author));
+        record.setUserid(author.getId());
+        record.setGuildname(event.getGuild().getName());
+        record.setGuildid(event.getGuild().getId());
+    }
+
+    private void executeCommand(GuildMessageReceivedEvent event, Command command, String commandName, String args) {
+        createCommandLog(event, command, commandName, args);
+        bot.getMetrics().increment("command.count");
+
+        long startTime = System.currentTimeMillis();
+        boolean showUsage = false;
+        try {
+            showUsage = command.run(bot, event, args);
+        } catch (Exception e) {
+            DiscordUtils.failReact(bot, event.getMessage());
+            DiscordUtils.sendMessage(event.getChannel(), "There was an error running your command, this incident has been logged.");
+            log.error(String.format("%s failed with arguments %s in guild %s - %s", commandName, args, event.getGuild().getName(), event.getGuild().getId()), e);
+        } finally {
+            long endTime = System.currentTimeMillis();
+            bot.getMetrics().increment(commandName + ".count");
+            bot.getMetrics().histogram(commandName + ".time", (int)(endTime - startTime));
+        }
+
+        if (showUsage) {
+            String[] usages = command.getUsages();
+            String prefix = DatabaseUtils.getGuildSetting(bot.getDatabase(), event.getGuild(), "prefix");
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setAuthor("Safety Jim - \"" + commandName + "\" Syntax", null, shard.getSelfUser().getAvatarUrl())
+                 .setDescription(DiscordUtils.getUsageString(prefix, usages))
+                 .setColor(new Color(0x4286F4));
+
+            DiscordUtils.failReact(bot, event.getMessage());
+            event.getChannel().sendMessage(embed.build()).queue();
+        }
     }
 }
