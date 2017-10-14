e7f62adb78c696f63d63acc4831ebbbc467400ff
Şamil NART <samil.nart@gmail.com>
2017-10-14 22:12:11 +0300

    Code cleanup

--- src/main/java/org/samoxive/safetyjim/discord/commands/Ping.java
+++ src/main/java/org/samoxive/safetyjim/discord/commands/Ping.java
@@ -1,6 +1,7 @@
 package org.samoxive.safetyjim.discord.commands;
 
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import org.samoxive.safetyjim.discord.Command;
 import org.samoxive.safetyjim.discord.DiscordBot;
@@ -16,10 +17,11 @@ public class Ping extends Command {
 
     @Override
     public boolean run(DiscordBot bot, GuildMessageReceivedEvent event, String args) {
+        JDA shard = event.getJDA();
         DiscordUtils.successReact(bot, event.getMessage());
         EmbedBuilder embed = new EmbedBuilder();
-        embed.setAuthor("Safety Jim " + event.getJDA().getShardInfo().getShardString(), null, event.getJDA().getSelfUser().getAvatarUrl());
-        embed.setDescription(":ping_pong: Ping: " + event.getJDA().getPing() + "ms");
+        embed.setAuthor("Safety Jim " + shard.getShardInfo().getShardString(), null, shard.getSelfUser().getAvatarUrl());
+        embed.setDescription(":ping_pong: Ping: " + shard.getPing() + "ms");
         embed.setColor(new Color(0x4286F4));
         DiscordUtils.sendMessage(event.getChannel(), embed.build());
         return false;
