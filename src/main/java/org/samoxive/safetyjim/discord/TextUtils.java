fdb532515a77da889062d9146c7e22f1e99836e4
Şamil NART <samil.nart@gmail.com>
2017-10-21 22:29:00 +0300

    Added a reason and date parser

---
+++ src/main/java/org/samoxive/safetyjim/discord/TextUtils.java
@@ -0,0 +1,69 @@
+package org.samoxive.safetyjim.discord;
+
+import com.joestelmach.natty.DateGroup;
+import com.joestelmach.natty.Parser;
+import org.samoxive.safetyjim.helpers.Pair;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class TextUtils {
+    /**
+     * Parses command arguments and returns a text and a date provided that
+     * the argument is in the form of "text | human time", the date argument
+     * has to be in the future
+     * @param scan
+     * @return
+     * @throws InvalidTimeInputException
+     * @throws TimeInputInPastException
+     */
+    public static Pair<String, Date> getTextAndTime(Scanner scan) throws InvalidTimeInputException, TimeInputInPastException {
+        String text = null;
+        String timeArgument = null;
+
+        if (scan.hasNext()) {
+            StringBuilder argumentsRaw = new StringBuilder();
+
+            while (scan.hasNextLine()) {
+                argumentsRaw.append(scan.nextLine());
+                argumentsRaw.append("\n");
+            }
+
+            String[] splitArgumentsRaw = argumentsRaw.toString().split("\\|");
+
+            if (splitArgumentsRaw.length == 1) {
+                text = splitArgumentsRaw[0];
+            } else {
+                text = splitArgumentsRaw[0];
+                timeArgument = splitArgumentsRaw[1];
+            }
+
+            text = text.trim();
+            timeArgument = timeArgument == null ? null: timeArgument.trim();
+        }
+
+        Date time = null;
+        Date now = new Date();
+
+        if (timeArgument != null) {
+            Parser parser = new Parser();
+            List<DateGroup> dateGroups = parser.parse(timeArgument);
+
+            try {
+                time = dateGroups.get(0).getDates().get(0);
+            } catch (IndexOutOfBoundsException e) {
+                throw new InvalidTimeInputException();
+            }
+
+            if (time.compareTo(now) < 0) {
+                throw new TimeInputInPastException();
+            }
+        }
+
+        return new Pair<>(text, time);
+    }
+
+    public static class InvalidTimeInputException extends Exception {}
+    public static class TimeInputInPastException extends Exception {}
+}
