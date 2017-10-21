d0512e90b98f3730b2789d8dc0e3838c948b2d34
Şamil NART <samil.nart@gmail.com>
2017-10-21 22:29:20 +0300

    Initial commit

---
+++ src/main/java/org/samoxive/safetyjim/helpers/Pair.java
@@ -0,0 +1,19 @@
+package org.samoxive.safetyjim.helpers;
+
+public class Pair<A, B> {
+    private A left;
+    private B right;
+
+    public Pair(A left, B right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public A getLeft() {
+        return left;
+    }
+
+    public B getRight() {
+        return right;
+    }
+}
