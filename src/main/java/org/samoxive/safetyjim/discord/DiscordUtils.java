6b9f0f0afb97f8e94e1606561a5531eb8d00137e
Şamil NART <samil.nart@gmail.com>
2017-10-21 19:37:00 +0300

    Added createModLogEntry, deleteCommandMessage, getChannelMention, getUserTagAndId, getHighestRole methods

--- src/main/java/org/samoxive/safetyjim/discord/DiscordUtils.java
+++ src/main/java/org/samoxive/safetyjim/discord/DiscordUtils.java
@@ -1,12 +1,16 @@
 package org.samoxive.safetyjim.discord;
 
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.managers.GuildController;
+import org.samoxive.safetyjim.database.DatabaseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
-import java.util.StringJoiner;
 import java.util.regex.Pattern;
 
 public class DiscordUtils {
@@ -23,6 +27,118 @@ public class DiscordUtils {
     public static final Pattern USER_MENTION_PATTERN = Pattern.compile("<@!?([0-9]+)>");
     public static final Pattern CHANNEL_MENTION_PATTERN = Pattern.compile("<#!?([0-9]+)>");
     public static final Pattern ROLE_MENTION_PATTERN = Pattern.compile("<@&!?([0-9]+)>");
+
+    public static final Map<String, Color> modLogColors = new HashMap<>();
+    static {
+        modLogColors.put("ban", new Color(0xFF2900));
+        modLogColors.put("kick", new Color(0xFF9900));
+        modLogColors.put("warn", new Color(0xFFEB00));
+        modLogColors.put("mute", new Color(0xFFFFFF));
+        modLogColors.put("softban", new Color(0xFF55DD));
+    }
+
+    public static final Map<String, String> modLogActionTexts = new HashMap<>();
+    static {
+        modLogActionTexts.put("ban", "Ban");
+        modLogActionTexts.put("softban", "Softban");
+        modLogActionTexts.put("kick", "Kick");
+        modLogActionTexts.put("warn", "Warn");
+        modLogActionTexts.put("mute", "Mute");
+    }
+
+    public static void createModLogEntry(DiscordBot bot, JDA shard, Message message, Member member, String reason, String action, int id, Date expirationDate, boolean expires) {
+        Map<String, String> guildSettings = DatabaseUtils.getGuildSettings(bot.getDatabase(), member.getGuild());
+        Date now = new Date();
+
+        boolean modLogActive = guildSettings.get("modlogactive").equals("true");
+        String prefix = guildSettings.get("prefix");
+
+        if (!modLogActive) {
+            return;
+        }
+
+        TextChannel modLogChannel = shard.getTextChannelById(guildSettings.get("modlogchannelid"));
+
+        if (modLogChannel == null) {
+            sendMessage(message.getChannel(), "Invalid moderator log channel in guild configuration, set a proper one via `" + prefix + " settings` command.");
+            return;
+        }
+
+        EmbedBuilder embed = new EmbedBuilder();
+        User user = member.getUser();
+        TextChannel channel = message.getTextChannel();
+        embed.setColor(modLogColors.get(action));
+        embed.addField("Action ", modLogActionTexts.get(action) + "- #" + id, false);
+        embed.addField("User:", getUserTagAndId(user), false);
+        embed.addField("Reason:", reason, false);
+        embed.addField("Responsible Moderator:", getUserTagAndId(message.getAuthor()), false);
+        embed.addField("Channel", getChannelMention(channel), false);
+        embed.setTimestamp(now.toInstant());
+
+        if (expires) {
+            String dateText = expirationDate == null ? "Indefinitely" : expirationDate.toString();
+            String untilText = null;
+
+            switch (action) {
+                case "ban":
+                    untilText = "Banned until";
+                    break;
+                case "mute":
+                    untilText = "Muted until";
+                    break;
+                default:
+                    break;
+            }
+
+            embed.addField(untilText, dateText, false);
+        }
+
+        sendMessage(modLogChannel, embed.build());
+    }
+
+    public static void deleteCommandMessage(DiscordBot bot, Message message) {
+        boolean silentCommandsActive = DatabaseUtils.getGuildSetting(bot.getDatabase(), message.getGuild(), "silentcommands").equals("true");
+
+        if (!silentCommandsActive) {
+            return;
+        }
+
+        try {
+            message.delete().complete();
+        } catch (Exception e) {
+            //
+        }
+    }
+    public static boolean isBannable(Member toBan, Member banner) {
+        Guild guild = toBan.getGuild();
+        User toBanUser = toBan.getUser();
+        User bannerUser = banner.getUser();
+
+        // Users can't ban themselves
+        if (bannerUser.getId().equals(toBanUser.getId())) {
+            return false;
+        }
+
+        // Owners cannot be banned
+        String ownerId = guild.getOwner().getUser().getId();
+        if (ownerId.equals(toBanUser.getId())) {
+            return false;
+        }
+
+        Role highestRoleToBan = getHighestRole(toBan);
+        Role highestRoleBanner = getHighestRole(banner);
+
+        // If either of these variables are null, this means they have no roles
+        // If the person we are trying to ban has no roles, there are two possibilities
+        // Either banner also doesn't have a role, in which case both users are equal
+        // and banner doesn't have the power to ban, or banner has a role which will
+        // always equal to being above the to be banned user
+        if (highestRoleToBan == null || highestRoleBanner == null) {
+            return highestRoleBanner != null;
+        }
+
+        return highestRoleToBan.getPosition() < highestRoleBanner.getPosition();
+    }
 
     public static boolean isBotFarm(Guild guild) {
         for (String botList: botListIds) {
@@ -80,7 +196,7 @@ public class DiscordUtils {
         }
     }
 
-    public static void sendMessage(TextChannel channel, String message) {
+    public static void sendMessage(MessageChannel channel, String message) {
         try {
             channel.sendMessage(message).queue();
         } catch (Exception e) {
@@ -88,12 +204,36 @@ public class DiscordUtils {
         }
     }
 
-    public static void sendMessage(TextChannel channel, MessageEmbed embed) {
+    public static void sendMessage(MessageChannel channel, MessageEmbed embed) {
         try {
             channel.sendMessage(embed).queue();
         } catch (Exception e) {
             //
         }
+    }
+
+    public static String getChannelMention(MessageChannel channel) {
+        return "<#" + channel.getId() + ">";
+    }
+
+    public static String getUserTagAndId(User user) {
+        return getTag(user) + " (" + user.getId() + ")";
+    }
+
+    public static Role getHighestRole(Member member) {
+        List<Role> roles = member.getRoles();
+
+        if (roles.size() == 0) {
+            return null;
+        }
+
+        return roles.stream().reduce(((prev, next) -> {
+            if (prev != null) {
+                return next.getPosition() > prev.getPosition() ? next : prev;
+            } else {
+                return next;
+            }
+        })).get();
     }
 
     public static String getUsageString(String prefix, String[] usages) {
