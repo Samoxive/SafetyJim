93210e393c25bb5601a80de88207c44f703e3eb0
Şamil NART <samil.nart@gmail.com>
2017-10-22 20:01:13 +0300

    Initial commit

---
+++ src/main/java/org/samoxive/safetyjim/discord/commands/Help.java
@@ -0,0 +1,63 @@
+package org.samoxive.safetyjim.discord.commands;
+
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import org.jooq.DSLContext;
+import org.samoxive.safetyjim.database.DatabaseUtils;
+import org.samoxive.safetyjim.discord.Command;
+import org.samoxive.safetyjim.discord.DiscordBot;
+import org.samoxive.safetyjim.discord.DiscordUtils;
+
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Help extends Command {
+    private String[] usages = { "help - lists all the available commands and their usage" };
+    private MessageEmbed embed = null;
+
+    private String getUsageTexts(DiscordBot bot, String prefix) {
+        StringJoiner joiner = new StringJoiner("\n");
+        HashMap<String, Command> commandList = bot.getCommands();
+
+        for (Map.Entry<String, Command> commandEntry: commandList.entrySet()) {
+            Command command = commandEntry.getValue();
+            String commandName = commandEntry.getKey();
+
+            for (String usage: command.getUsages()) {
+                joiner.add(String.format("`%s %s` - %s", prefix, commandName, usage));
+            }
+        }
+
+        return joiner.toString();
+    }
+
+    @Override
+    public String[] getUsages() {
+        return usages;
+    }
+
+    @Override
+    public boolean run(DiscordBot bot, GuildMessageReceivedEvent event, String args) {
+        if (embed == null) {
+            JDA shard = event.getJDA();
+            DSLContext database = bot.getDatabase();
+            Guild guild = event.getGuild();
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.setAuthor("Safety Jim - Commands", null, shard.getSelfUser().getAvatarUrl());
+            builder.setDescription(getUsageTexts(bot, DatabaseUtils.getGuildSetting(database, guild, "prefix")));
+            builder.setColor(new Color(0x4286F4));
+
+            embed = builder.build();
+        }
+
+        DiscordUtils.successReact(bot, event.getMessage());
+        DiscordUtils.sendMessage(event.getChannel(), embed);
+        return false;
+    }
+}
