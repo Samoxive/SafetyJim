77c1d90fc2e5cbc4d5befdd2fa46b4f7e60f0886
Şamil NART <samil.nart@gmail.com>
2017-10-25 22:40:04 +0300

    Initial commit

---
+++ src/main/java/org/samoxive/safetyjim/discord/commands/Unban.java
@@ -0,0 +1,85 @@
+package org.samoxive.safetyjim.discord.commands;
+
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.managers.GuildController;
+import org.jooq.DSLContext;
+import org.jooq.Result;
+import org.samoxive.jooq.generated.Tables;
+import org.samoxive.jooq.generated.tables.records.BanlistRecord;
+import org.samoxive.safetyjim.discord.Command;
+import org.samoxive.safetyjim.discord.DiscordBot;
+import org.samoxive.safetyjim.discord.DiscordUtils;
+import org.samoxive.safetyjim.discord.TextUtils;
+import java.util.List;
+import java.util.Scanner;
+
+public class Unban extends Command {
+    private String[] usages = { "unban <tag> - unbans user with specified user tag (example#1998)" };
+
+    @Override
+    public String[] getUsages() {
+        return usages;
+    }
+
+    @Override
+    public boolean run(DiscordBot bot, GuildMessageReceivedEvent event, String args) {
+        Scanner messageIterator = new Scanner(args);
+
+        Member member = event.getMember();
+        Message message = event.getMessage();
+        Guild guild = event.getGuild();
+        Member selfMember = guild.getSelfMember();
+        GuildController controller = guild.getController();
+
+
+        if (!member.hasPermission(Permission.BAN_MEMBERS)) {
+            DiscordUtils.failMessage(bot, message, "You don't have enough permissions to execute this command!");
+            return false;
+        }
+
+        if (!selfMember.hasPermission(Permission.BAN_MEMBERS)) {
+            DiscordUtils.failMessage(bot, message, "I do not have enough permissions to do that!");
+            return false;
+        }
+
+        String unbanArgument = TextUtils.seekScannerToEnd(messageIterator);
+
+        if (unbanArgument.equals("")) {
+            return true;
+        }
+
+        List<User> bans = guild.getBans().complete();
+
+        User targetUser = bans.stream()
+                              .filter((bannedUser) -> {
+                                  String tag = DiscordUtils.getTag(bannedUser);
+                                  return tag.equals(unbanArgument);
+                              })
+                              .findAny()
+                              .orElse(null);
+
+        if (targetUser == null) {
+            DiscordUtils.failMessage(bot, message, "Could not find a banned user called `" + unbanArgument + "`!");
+            return false;
+        }
+
+        controller.unban(targetUser).complete();
+        DSLContext database = bot.getDatabase();
+
+        Result<BanlistRecord> records = database.selectFrom(Tables.BANLIST)
+                                                .where(Tables.BANLIST.GUILDID.eq(guild.getId()))
+                                                .and(Tables.BANLIST.USERID.eq(targetUser.getId()))
+                                                .fetch();
+
+        for (BanlistRecord record: records) {
+            record.setUnbanned(true);
+            record.update();
+        }
+
+        DiscordUtils.successReact(bot, message);
+
+        return false;
+    }
+}
