c987917631ada10a864d23ecf9739d7f59fb2c60
Şamil NART <samil.nart@gmail.com>
2017-10-28 17:19:38 +0300

    Added uptime metrics

--- src/main/java/org/samoxive/safetyjim/discord/DiscordBot.java
+++ src/main/java/org/samoxive/safetyjim/discord/DiscordBot.java
@@ -76,6 +76,7 @@ public class DiscordBot {
         scheduler.scheduleAtFixedRate(() -> unmuteUsers(), 1, 10, TimeUnit.SECONDS);
         scheduler.scheduleAtFixedRate(() -> unbanUsers(), 1, 30, TimeUnit.SECONDS);
         scheduler.scheduleAtFixedRate(() -> remindReminders(), 1, 5, TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(() -> metrics.gauge("uptime", (int)((new Date()).getTime() - startTime.getTime()) / (1000 * 60 * 60)), 1, 30, TimeUnit.MINUTES);
         scheduler.schedule(() -> updateBotLists(), 10, TimeUnit.SECONDS);
 
         String inviteLink = shards.get(0).getShard().asBot().getInviteUrl(
