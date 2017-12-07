c77ae43f51a5ac14b86bfa5e5ccc7134fa778fb8
Şamil NART <samil.nart@gmail.com>
2017-12-07 16:02:33 +0300

    Added getUserGuilds method

--- src/main/java/org/samoxive/safetyjim/discord/DiscordApiUtils.java
+++ src/main/java/org/samoxive/safetyjim/discord/DiscordApiUtils.java
@@ -1,10 +1,15 @@
 package org.samoxive.safetyjim.discord;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import okhttp3.*;
 import org.samoxive.safetyjim.config.Config;
 import org.samoxive.safetyjim.discord.entities.DiscordSecrets;
+import org.samoxive.safetyjim.discord.entities.PartialGuild;
 import org.samoxive.safetyjim.discord.entities.SelfUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiscordApiUtils {
     private static OkHttpClient client = new OkHttpClient();
@@ -60,6 +65,29 @@ public class DiscordApiUtils {
             }
 
             return gson.fromJson(userJson, SelfUser.class);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static List<PartialGuild> getUserGuilds(String accessToken) {
+        Request request = (new Request.Builder())
+                .url("https://discordapp.com/api/users/@me/guilds")
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .addHeader("User-Agent", "Safety Jim")
+                .get()
+                .build();
+
+        try {
+            ResponseBody responseBody = client.newCall(request)
+                                              .execute()
+                                              .body();
+            String guildsJson = responseBody.string();
+            if (guildsJson == null) {
+                return null;
+            }
+
+            return gson.fromJson(guildsJson, new TypeToken<ArrayList<PartialGuild>>(){}.getType());
         } catch (Exception e) {
             return null;
         }
