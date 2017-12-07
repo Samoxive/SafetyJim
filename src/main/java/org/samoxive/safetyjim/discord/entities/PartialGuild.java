2b18ed68be1963812568fe734539ab78439edc2c
Şamil NART <samil.nart@gmail.com>
2017-12-07 16:02:54 +0300

    Initial commit

---
+++ src/main/java/org/samoxive/safetyjim/discord/entities/PartialGuild.java
@@ -0,0 +1,9 @@
+package org.samoxive.safetyjim.discord.entities;
+
+public class PartialGuild {
+    public String id;
+    public String name;
+    public String icon;
+    public boolean owner;
+    public long permissions;
+}
