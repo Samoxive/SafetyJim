6cee8b08a8992adf9313aa5dd9040d680e9e9b7e
Şamil NART <samil.nart@gmail.com>
2017-12-09 22:22:32 +0300

    Remove root route, add headers for CORS

--- src/main/java/org/samoxive/safetyjim/server/Server.java
+++ src/main/java/org/samoxive/safetyjim/server/Server.java
@@ -1,13 +1,14 @@
 package org.samoxive.safetyjim.server;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import org.jooq.DSLContext;
 import org.samoxive.safetyjim.config.Config;
 import org.samoxive.safetyjim.discord.DiscordBot;
+import org.samoxive.safetyjim.server.routes.Guilds;
 import org.samoxive.safetyjim.server.routes.Login;
-import org.samoxive.safetyjim.server.routes.Root;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,14 +26,24 @@ public class Server {
 
         vertx = Vertx.vertx();
         Router router = Router.router(vertx);
+
         router.route().handler(BodyHandler.create());
 
-        router.route("/").handler(new Root(bot, database, this, config));
-        router.route("/login").handler(new Login(bot, database, this, config));
-
+        router.get("/login").handler(new Login(bot, database, this, config));
+        router.get("/guilds").handler(new Guilds(bot, database, this, config));
+        router.options().handler((ctx) -> {
+            HttpServerResponse response = ctx.response();
+            response.putHeader("Access-Control-Allow-Origin", config.server.base_url);
+            response.putHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            response.putHeader("Access-Control-Allow-Headers", "token");
+            response.end();
+        });
 
         vertx.createHttpServer()
-             .requestHandler(router::accept)
+             .requestHandler((request) -> {
+                 request.response().putHeader("Access-Control-Allow-Origin", config.server.base_url);
+                 router.accept(request);
+             })
              .listen(config.server.port, "0.0.0.0");
         log.info("Started web server.");
 
