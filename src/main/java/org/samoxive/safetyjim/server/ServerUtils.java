ce96cc9d476952c41731c0b036f568bf217df795
Şamil NART <samil.nart@gmail.com>
2017-12-09 22:23:07 +0300

    Add getIdFromToken method

--- src/main/java/org/samoxive/safetyjim/server/ServerUtils.java
+++ src/main/java/org/samoxive/safetyjim/server/ServerUtils.java
@@ -1,11 +1,27 @@
 package org.samoxive.safetyjim.server;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import io.vertx.core.http.HttpServerResponse;
 import org.samoxive.safetyjim.config.Config;
 
 import java.net.URLEncoder;
 
 public class ServerUtils {
+    public static String getIdFromToken(Config config, String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC512(config.server.secret);
+            JWTVerifier verifier = JWT.require(algorithm)
+                                      .build();
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT.getClaim("userId").asString();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     public static void redirectToDiscord(HttpServerResponse response, Config config) {
         try {
             response.setStatusCode(302);
