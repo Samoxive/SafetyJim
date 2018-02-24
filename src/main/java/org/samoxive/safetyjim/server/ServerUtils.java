a27a0712b95c4ce8351dc185693ff135f4318e9d
Şamil NART <samil.nart@gmail.com>
2018-02-25 00:32:08 +0300

    Add helper methods to remove some ceremony around error validating to get the member of a guild and process from and to parameters for stats routes

--- src/main/java/org/samoxive/safetyjim/server/ServerUtils.java
+++ src/main/java/org/samoxive/safetyjim/server/ServerUtils.java
@@ -4,13 +4,23 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.gson.Gson;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
 import org.samoxive.safetyjim.config.Config;
+import org.samoxive.safetyjim.discord.DiscordBot;
+import org.samoxive.safetyjim.discord.DiscordShard;
+import org.samoxive.safetyjim.discord.DiscordUtils;
+import org.samoxive.safetyjim.helpers.Pair;
 
 import java.net.URLEncoder;
 
 public class ServerUtils {
+    public static Gson gson = new Gson();
+
     public static String authUser(HttpServerRequest request, HttpServerResponse response, Config config) {
         String token = request.getHeader("token");
         if (token == null) {
@@ -29,6 +39,7 @@ public class ServerUtils {
 
         return userId;
     }
+
     public static String getIdFromToken(Config config, String token) {
         try {
             Algorithm algorithm = Algorithm.HMAC512(config.server.secret);
@@ -37,6 +48,71 @@ public class ServerUtils {
             DecodedJWT decodedJWT = verifier.verify(token);
             return decodedJWT.getClaim("userId").asString();
         } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static Member getMember(DiscordBot bot, HttpServerRequest request, HttpServerResponse response, Config config) {
+        String userId = authUser(request, response, config);
+        if (userId == null) {
+            response.setStatusCode(401);
+            return null;
+        }
+
+        String guildId = request.getParam("guildId");
+        long guildIdLong;
+        try {
+            guildIdLong = Long.parseLong(guildId);
+        } catch (Exception e) {
+            response.setStatusCode(400);
+            response.end();
+            return null;
+        }
+
+        int shardId = DiscordUtils.getShardIdFromGuildId(guildIdLong, config.jim.shard_count);
+        JDA shard = bot.getShards()
+                       .get(shardId)
+                       .getShard();
+
+        Guild guild = shard.getGuildById(guildIdLong);
+        if (guild == null) {
+            response.setStatusCode(404);
+            response.end();
+            return null;
+        }
+
+        Member member = guild.getMemberById(userId);
+        if (member == null) {
+            response.setStatusCode(404);
+            response.end();
+            return null;
+        }
+
+        return member;
+    }
+
+    public static Pair<Long, Long> validateFromAndTo(HttpServerRequest request, HttpServerResponse response) {
+        String fromParam = request.getParam("from");
+        String toParam = request.getParam("to");
+
+        if (fromParam == null || toParam == null) {
+            response.setStatusCode(400);
+            response.end();
+            return null;
+        }
+
+        try {
+            long from = Long.parseLong(fromParam);
+            long to = Long.parseLong(toParam);
+
+            if (from <= 0 || to <= 0 || from >= to) {
+                throw new Exception();
+            } else {
+                return new Pair<>(from, to);
+            }
+        } catch (Exception e) {
+            response.setStatusCode(400);
+            response.end();
             return null;
         }
     }
