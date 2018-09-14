51b10ff9759d74f957414ff51a4d9675aa50c062
Şamil NART <samil.nart@gmail.com>
2018-09-14 18:25:41 +0300

    Add server related utilities

---
+++ src/main/java/org/samoxive/safetyjim/server/Utils.kt
@@ -0,0 +1,34 @@
+package org.samoxive.safetyjim.server
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.uchuhimo.konf.Config
+import io.vertx.core.http.HttpServerResponse
+import kotlinx.coroutines.experimental.DefaultDispatcher
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.withContext
+import okhttp3.Call
+import org.samoxive.safetyjim.await
+import org.samoxive.safetyjim.config.ServerConfig
+import org.samoxive.safetyjim.tryhard
+
+fun getUserIdFromToken(config: Config, token: String) = tryhard {
+    val algorithm = Algorithm.HMAC512(config[ServerConfig.secret])
+    val verifier = JWT.require(algorithm).build()
+    val decodedToken = verifier.verify(token)
+    decodedToken.getClaim("userId").asString()
+}
+
+fun getJWTFromUserId(config: Config, userId: String): String {
+    val algorithm = Algorithm.HMAC512(config[ServerConfig.secret])
+    return JWT.create()
+            .withClaim("userId", userId)
+            .sign(algorithm)
+}
+
+suspend fun Call.asyncExecute() = await { execute() }
+fun HttpServerResponse.endJson(string: String) {
+    putHeader("Content-Type", "application/json")
+    end("\"$string\"")
+}
