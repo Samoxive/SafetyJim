f6ffcca04d85820e09058a331da79e3054372e1f
Şamil NART <samil.nart@gmail.com>
2018-09-14 18:12:24 +0300

    Add async utilities

--- src/main/java/org/samoxive/safetyjim/Utils.kt
+++ src/main/java/org/samoxive/safetyjim/Utils.kt
@@ -1,8 +1,17 @@
 package org.samoxive.safetyjim
 
+import com.mashape.unirest.request.BaseRequest
+import kotlinx.coroutines.experimental.DefaultDispatcher
+import kotlinx.coroutines.experimental.withContext
 import org.slf4j.Logger
 
 fun <T> tryhard(block: () -> T): T? = try {
+    block()
+} catch (e: Throwable) {
+    null
+}
+
+suspend fun <T> tryhardAsync(block: suspend () -> T): T? = try {
     block()
 } catch (e: Throwable) {
     null
@@ -14,3 +23,6 @@ fun tryAndLog(log: Logger, functionName: String, block: () -> Any) = try {
     log.error("Exception occured in $functionName", e)
 }
 
+suspend fun <T> await(block: suspend () -> T): T = withContext(DefaultDispatcher, block = block)
+
+suspend fun BaseRequest.awaitAsString() = await { asString() }
