88c7b629d4b1bb5feb73157172e9fb877649edb4
Şamil NART <samil.nart@gmail.com>
2019-02-09 16:11:14 +0300

    Make scheduled functions suspending

--- src/main/java/org/samoxive/safetyjim/discord/DiscordBot.kt
+++ src/main/java/org/samoxive/safetyjim/discord/DiscordBot.kt
@@ -1,28 +1,25 @@
 package org.samoxive.safetyjim.discord
 
-import com.mashape.unirest.http.Unirest
 import com.uchuhimo.konf.Config
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import net.dv8tion.jda.core.EmbedBuilder
 import net.dv8tion.jda.core.MessageBuilder
 import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.entities.Guild
 import net.dv8tion.jda.core.utils.SessionControllerAdapter
 import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.transactions.transaction
-import org.json.JSONObject
-import org.samoxive.safetyjim.config.BotListConfig
 import org.samoxive.safetyjim.config.JimConfig
 import org.samoxive.safetyjim.database.*
 import org.samoxive.safetyjim.discord.commands.*
 import org.samoxive.safetyjim.discord.processors.InviteLink
 import org.samoxive.safetyjim.discord.processors.MessageStats
-import org.samoxive.safetyjim.tryAndLog
 import org.samoxive.safetyjim.tryhard
+import org.samoxive.safetyjim.tryhardAsync
 import org.slf4j.LoggerFactory
 import java.awt.Color
 import java.util.*
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 
 class DiscordBot(val config: Config) {
@@ -52,7 +49,6 @@ class DiscordBot(val config: Config) {
     )
     val deleteCommands = arrayOf("ban", "kick", "mute", "softban", "warn", "hardban")
     val processors = listOf(InviteLink(), MessageStats())
-    private val scheduler = Executors.newScheduledThreadPool(8)
     val startTime = Date()
 
     val guildCount: Long
@@ -70,12 +66,12 @@ class DiscordBot(val config: Config) {
             Thread.sleep(5000)
         }
 
-        scheduler.scheduleAtFixedRate({ tryAndLog(log, "allowUsers") { allowUsers() } }, 10, 5, TimeUnit.SECONDS)
-        scheduler.scheduleAtFixedRate({ tryAndLog(log, "unmuteUsers") { unmuteUsers() } }, 10, 10, TimeUnit.SECONDS)
-        scheduler.scheduleAtFixedRate({ tryAndLog(log, "unbanUsers") { unbanUsers() } }, 10, 30, TimeUnit.SECONDS)
-        scheduler.scheduleAtFixedRate({ tryAndLog(log, "remindReminders") { remindReminders() } }, 10, 5, TimeUnit.SECONDS)
-        scheduler.scheduleAtFixedRate({ tryAndLog(log, "saveMemberCounts") { saveMemberCounts() } }, 1, 10, TimeUnit.MINUTES)
-        scheduler.scheduleAtFixedRate({ tryAndLog(log, "updateBotLists") { updateBotLists() } }, 10, 1, TimeUnit.MINUTES)
+        scheduleJob(10, 5, "allowUsers") { allowUsers() }
+        scheduleJob(10, 5, "allowUsers") { allowUsers() }
+        scheduleJob(10, 10, "unmuteUsers") { unmuteUsers() }
+        scheduleJob(10, 30, "unbanUsers") { unbanUsers() }
+        scheduleJob(10, 5, "remindReminders") { remindReminders() }
+        scheduleJob(10, 10 * 60, "saveMemberCounts") { saveMemberCounts() }
 
         val inviteLink = shards[0].jda.asBot().getInviteUrl(
                 Permission.KICK_MEMBERS,
@@ -90,39 +86,56 @@ class DiscordBot(val config: Config) {
         log.info("Bot invite link: $inviteLink")
     }
 
+    private fun scheduleJob(delaySeconds: Int, intervalSeconds: Int, jobName: String, job: suspend () -> Unit) {
+        GlobalScope.launch {
+            delay(delaySeconds * 1000L)
+            while (true) {
+                try {
+                    job()
+                } catch (e: Throwable) {
+                    log.error("Exception occured in $jobName", e)
+                }
+                delay(intervalSeconds * 1000L)
+            }
+        }
+    }
+
     fun getGuildFromBot(guildId: String): Guild? {
         val guildIdLong = tryhard { guildId.toLong() } ?: return null
         val shardId = getShardIdFromGuildId(guildIdLong, shards.size)
         return shards[shardId].jda.getGuildById(guildId)
     }
 
-    private fun saveMemberCounts() = transaction {
+    private suspend fun saveMemberCounts() {
         val settings = getAllGuildSettings()
         shards.map { shard -> shard.jda.guilds }
-                .flatMap { it }
+                .flatten()
                 .filter { guild -> settings[guild.idLong]?.statistics ?: false }
                 .forEach { guild ->
                     val onlineCount = guild.members.asSequence().filter { member -> member.isOnline() }.count()
-                    JimMemberCount.new {
-                        guildid = guild.idLong
-                        date = Date().time
-                        onlinecount = onlineCount
-                        count = guild.members.size
+                    awaitTransaction {
+                        JimMemberCount.new {
+                            guildid = guild.idLong
+                            date = Date().time
+                            onlinecount = onlineCount
+                            count = guild.members.size
+                        }
                     }
                 }
     }
 
-    private fun allowUsers() = transaction {
+    private suspend fun allowUsers() {
         val currentTime = System.currentTimeMillis() / 1000
 
-        val usersToBeAllowed = JimJoin.find {
-            (JimJoinTable.allowed eq false) and (JimJoinTable.allowtime less currentTime)
+        val usersToBeAllowed = awaitTransaction {
+            JimJoin.find {
+                (JimJoinTable.allowed eq false) and (JimJoinTable.allowtime less currentTime)
+            }.toList()
         }
 
         for (user in usersToBeAllowed) {
             val guildId = user.guildid
-            val guildIdLong = guildId.toLong()
-            val shardId = getShardIdFromGuildId(guildIdLong, config[JimConfig.shard_count])
+            val shardId = getShardIdFromGuildId(guildId, config[JimConfig.shard_count])
             val shard = shards[shardId]
             val shardClient = shard.jda
             val guild = shardClient.getGuildById(guildId)
@@ -136,81 +149,79 @@ class DiscordBot(val config: Config) {
             val enabled = guildSettings.holdingroom
 
             if (enabled) {
-                val guildUser = shard.jda.retrieveUserById(user.userid).complete()
+                val guildUser = shard.jda.retrieveUserById(user.userid).await()
                 val member = guild.getMember(guildUser)
                 val roleId = guildSettings.holdingroomroleid
                 val role = if (roleId != null) guild.getRoleById(roleId) else null
                 val controller = guild.controller
 
                 if (role == null) {
-                    guildSettings.holdingroom = false
+                    awaitTransaction { guildSettings.holdingroom = false }
                     continue
                 }
 
-                tryhard {
-                    controller.addSingleRoleToMember(member, role).complete()
-                }
-                user.allowed = true
-            } else {
-                user.allowed = true
+                controller.addSingleRoleToMember(member, role).tryAwait()
             }
+
+            awaitTransaction { user.allowed = true }
         }
     }
 
-    private fun unbanUsers() = transaction {
+    private suspend fun unbanUsers() {
         val currentTime = System.currentTimeMillis() / 1000
 
-        val usersToBeUnbanned = JimBan.find {
-            (JimBanTable.unbanned eq false) and (JimBanTable.expires eq true) and (JimBanTable.expiretime less currentTime)
+        val usersToBeUnbanned = awaitTransaction {
+            JimBan.find {
+                (JimBanTable.unbanned eq false) and (JimBanTable.expires eq true) and (JimBanTable.expiretime less currentTime)
+            }.toList()
         }
 
         for (user in usersToBeUnbanned) {
             val guildId = user.guildid
-            val guildIdLong = guildId.toLong()
-            val shardId = getShardIdFromGuildId(guildIdLong, config[JimConfig.shard_count])
+            val shardId = getShardIdFromGuildId(guildId, config[JimConfig.shard_count])
             val shard = shards[shardId]
             val shardClient = shard.jda
             val guild = shardClient.getGuildById(guildId)
 
             if (guild == null) {
-                user.unbanned = true
+                awaitTransaction { user.unbanned = true }
                 continue
             }
 
-            val guildUser = shard.jda.retrieveUserById(user.userid).complete()
+            val guildUser = shard.jda.retrieveUserById(user.userid).await()
             val controller = guild.controller
 
             if (!guild.selfMember.hasPermission(Permission.BAN_MEMBERS)) {
-                user.unbanned = true
+                awaitTransaction { user.unbanned = true }
                 continue
             }
 
             val banRecord = guild.banList
-                    .complete()
-                    .firstOrNull { ban -> ban.user.id == guildUser.id }
+                    .tryAwait()
+                    ?.firstOrNull { ban -> ban.user.id == guildUser.id }
 
             if (banRecord == null) {
-                user.unbanned = true
+                awaitTransaction { user.unbanned = true }
+                continue
             }
 
-            tryhard {
-                controller.unban(guildUser).complete()
-            }
-            user.unbanned = true
+            controller.unban(guildUser).tryAwait()
+            awaitTransaction { user.unbanned = true }
         }
     }
 
-    private fun unmuteUsers() = transaction {
+    private suspend fun unmuteUsers() {
         val currentTime = System.currentTimeMillis() / 1000
 
-        val usersToBeUnmuted = JimMute.find {
-            (JimMuteTable.unmuted eq false) and (JimMuteTable.expires eq true) and (JimMuteTable.expiretime less currentTime)
+        val usersToBeUnmuted = awaitTransaction {
+            JimMute.find {
+                (JimMuteTable.unmuted eq false) and (JimMuteTable.expires eq true) and (JimMuteTable.expiretime less currentTime)
+            }.toList()
         }
 
         for (user in usersToBeUnmuted) {
             val guildId = user.guildid
-            val guildIdLong = guildId.toLong()
-            val shardId = getShardIdFromGuildId(guildIdLong, config[JimConfig.shard_count])
+            val shardId = getShardIdFromGuildId(guildId, config[JimConfig.shard_count])
             val shard = shards[shardId]
             val shardClient = shard.jda
             val guild = shardClient.getGuildById(guildId)
@@ -220,53 +231,52 @@ class DiscordBot(val config: Config) {
                 continue
             }
 
-            val guildUser = shard.jda.retrieveUserById(user.userid).complete()
+            val guildUser = shard.jda.retrieveUserById(user.userid).await()
             val member = guild.getMember(guildUser)
             if (member == null) {
-                user.unmuted = true
+                awaitTransaction { user.unmuted = true }
                 continue
             }
 
             val mutedRoles = guild.getRolesByName("Muted", false)
             val role = if (mutedRoles.isEmpty()) {
-                tryhard { Mute.setupMutedRole(guild) }
+                tryhardAsync { Mute.setupMutedRole(guild) }
             } else {
                 mutedRoles[0]
             }
 
             if (role == null) {
-                user.unmuted = true
+                awaitTransaction { user.unmuted = true }
                 continue
             }
 
             val controller = guild.controller
 
-            tryhard {
-                controller.removeSingleRoleFromMember(member, role).complete()
-            }
-            user.unmuted = true
+            controller.removeSingleRoleFromMember(member, role).tryAwait()
+            awaitTransaction { user.unmuted = true }
         }
     }
 
-    private fun remindReminders() = transaction {
+    private suspend fun remindReminders() {
         val now = Date().time / 1000
 
-        val reminders = JimReminder.find {
-            (JimReminderTable.reminded eq false) and (JimReminderTable.remindtime less now)
+        val reminders = awaitTransaction {
+            JimReminder.find {
+                (JimReminderTable.reminded eq false) and (JimReminderTable.remindtime less now)
+            }.toList()
         }
 
         for (reminder in reminders) {
             val guildId = reminder.guildid
-            val guildIdLong = guildId.toLong()
             val channelId = reminder.channelid
             val userId = reminder.userid
-            val shardId = getShardIdFromGuildId(guildIdLong, config[JimConfig.shard_count])
+            val shardId = getShardIdFromGuildId(guildId, config[JimConfig.shard_count])
             val shard = shards[shardId].jda
             val guild = shard.getGuildById(guildId)
-            val user = shard.retrieveUserById(userId).complete()
+            val user = shard.retrieveUserById(userId).await()
 
             if (guild == null) {
-                reminder.reminded = true
+                awaitTransaction { reminder.reminded = true }
                 continue
             }
 
@@ -282,36 +292,20 @@ class DiscordBot(val config: Config) {
             embed.setColor(Color(0x4286F4))
 
             if (channel == null || member == null) {
-                user.sendDM(embed.build())
+                user.trySendMessage(embed.build())
             } else {
                 try {
                     val builder = MessageBuilder()
                     builder.append(user.asMention)
                     builder.setEmbed(embed.build())
 
-                    channel.sendMessage(builder.build()).complete()
+                    channel.sendMessage(builder.build()).await()
                 } catch (e: Exception) {
-                    user.sendDM(embed.build())
+                    user.trySendMessage(embed.build())
                 }
             }
 
-            reminder.reminded = true
-        }
-    }
-
-    private fun updateBotLists() {
-        if (!config[BotListConfig.enabled]) {
-            return
-        }
-
-        val guildCount = guildCount
-        val clientId = shards[0].jda.selfUser.id
-        for (list in config[BotListConfig.list]) {
-            val body = JSONObject().put("server_count", guildCount)
-            Unirest.post(list.url.replace("\$id", clientId))
-                    .header("Authorization", list.token)
-                    .body(body)
-                    .asBinaryAsync()
+            awaitTransaction { reminder.reminded = true }
         }
     }
 }
