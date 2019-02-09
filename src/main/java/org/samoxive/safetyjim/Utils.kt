ae59e6fabffbf6e8c14a05ed4e360f8fb722de58
Şamil NART <samil.nart@gmail.com>
2019-02-09 15:54:37 +0300

    Add coroutine convenience methods for Unirest

--- src/main/java/org/samoxive/safetyjim/Utils.kt
+++ src/main/java/org/samoxive/safetyjim/Utils.kt
@@ -1,7 +1,15 @@
 package org.samoxive.safetyjim
 
+import com.mashape.unirest.http.HttpResponse
+import com.mashape.unirest.http.JsonNode
+import com.mashape.unirest.http.async.Callback
+import com.mashape.unirest.http.exceptions.UnirestException
 import com.mashape.unirest.request.BaseRequest
 import org.slf4j.Logger
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 fun <T> tryhard(block: () -> T): T? = try {
     block()
@@ -23,4 +31,42 @@ fun tryAndLog(log: Logger, functionName: String, block: () -> Any) = try {
 
 suspend fun <T> await(block: suspend () -> T): T = block()
 
-suspend fun BaseRequest.awaitAsString() = await { asString() }!!
+suspend fun BaseRequest.awaitAsString(): HttpResponse<String> = suspendCoroutine { cont ->
+    asStringAsync(object: Callback<String> {
+        override fun cancelled() {
+            throw NotImplementedError("don't cancel requests, we don't need to")
+        }
+
+        override fun completed(response: HttpResponse<String>) {
+            cont.resume(response)
+        }
+
+        override fun failed(e: UnirestException) {
+            cont.resumeWithException(e)
+        }
+    })
+}
+
+suspend fun BaseRequest.tryAwaitAsString(): HttpResponse<String>? = tryhardAsync {
+    awaitAsString()
+}
+
+suspend fun BaseRequest.awaitAsJSON(): HttpResponse<JsonNode> = suspendCoroutine { cont ->
+    asJsonAsync(object: Callback<JsonNode> {
+        override fun cancelled() {
+            throw NotImplementedError("don't cancel requests, we don't need to")
+        }
+
+        override fun completed(response: HttpResponse<JsonNode>) {
+            cont.resume(response)
+        }
+
+        override fun failed(e: UnirestException) {
+            cont.resumeWithException(e)
+        }
+    })
+}
+
+suspend fun BaseRequest.tryAwaitAsJSON(): HttpResponse<JsonNode>? = tryhardAsync {
+    awaitAsJSON()
+}
