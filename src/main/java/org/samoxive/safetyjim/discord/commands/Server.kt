bac89ace641c2738ecffd2a8ff7a9da6ba6e1d6b
Şamil NART <samil.nart@gmail.com>
2019-12-16 15:25:50 +0300

    Remove member count and emojis from server command

--- src/main/java/org/samoxive/safetyjim/discord/commands/Server.kt
+++ src/main/java/org/samoxive/safetyjim/discord/commands/Server.kt
@@ -5,38 +5,23 @@ import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import org.samoxive.safetyjim.database.SettingsEntity
 import org.samoxive.safetyjim.discord.*
+import org.samoxive.safetyjim.tryhardAsync
 
 class Server : Command() {
     override val usages = arrayOf("server - displays information about the current server")
 
     override suspend fun run(bot: DiscordBot, event: GuildMessageReceivedEvent, settings: SettingsEntity, args: String): Boolean {
         val guild = event.guild
-        val owner = guild.owner?.user
+        val owner = tryhardAsync { guild.retrieveOwner().await() }?.user
         val channel = event.channel
         val message = event.message
-        val memberCount = guild.memberCache.size()
         val creationDate = guild.timeCreated.toLocalDate().toString()
-        val emojis = StringBuilder()
-
-        for (emote in guild.retrieveEmotes().await()) {
-            if (emojis.length > 950) {
-                emojis.append("...")
-                break
-            } else {
-                emojis.append(emote.asMention)
-            }
-        }
-
-        var emojiString = emojis.toString()
-        emojiString = if (emojiString == "") "None" else emojiString
 
         val embed = EmbedBuilder()
         embed.setAuthor(guild.name, null, guild.iconUrl)
         embed.setColor(Color(0x4286F4))
         embed.addField("Server Owner", owner?.getTag(), true)
-        embed.addField("Member Count", memberCount.toString(), true)
         embed.addField("Creation Date", creationDate, true)
-        embed.addField("Emojis", emojiString, false)
 
         message.successReact()
         channel.trySendMessage(embed.build())
