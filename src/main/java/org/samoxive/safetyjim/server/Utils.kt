fc876d3a40a58f64e4b03c698eaed8f46402fe03
Şamil NART <samil.nart@gmail.com>
2019-12-16 11:22:52 +0300

    Ignore unknown keys when parsing json (discord can change their api)

--- src/main/java/org/samoxive/safetyjim/server/Utils.kt
+++ src/main/java/org/samoxive/safetyjim/server/Utils.kt
@@ -2,6 +2,7 @@ package org.samoxive.safetyjim.server
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.uchuhimo.konf.Config
 import io.vertx.core.http.HttpServerResponse
@@ -14,6 +15,7 @@ import org.samoxive.safetyjim.database.UUIDBlacklistTable
 import org.samoxive.safetyjim.tryhardAsync
 
 val objectMapper = jacksonObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
 suspend fun getUserIdFromToken(config: Config, token: String) = tryhardAsync {
     val algorithm = Algorithm.HMAC512(config[ServerConfig.secret])
